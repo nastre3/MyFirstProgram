3884275a7a6dba4bc687fce3337f1440beb5d8cd
Anastasia <nastre70@gmail.com>
2021-11-24 14:38:02 +1000

    Added classes Square, Rectangle, methods

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -2,23 +2,17 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hi("stranger"); // конкретное значение параметра
         hi("user");
-        double len = 5;
-        System.out.println("S квадрата со стороной " + len + " = " + calculateArea(len));
-        double a = 4;
-        double b = 6;
-        System.out.println("S прямоугольника со сторонами " + a + " и " + b + " = " + calculateArea(a, b));
 
-    }
+        Square s = new Square(5);
+        System.out.println("S квадрата со стороной " + s.l + " = " + s.calculateArea());
 
-    private static double calculateArea(double a, double b) {
-        return a * b;
-    }
+        Rectangle r = new Rectangle(3, 4);
+        System.out.println("S прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.calculateArea());
 
-    private static double calculateArea(double l) {
-        return l * l;
     }
 
     private static void hi(String somebody) { // somebody - параметр/аргумент ф-ии; void - ф-ия не возвращает значение
         System.out.println("Hi " + somebody);
     }
+
 }
